147266ce069bbcf3f32cc12fc876a94bfc77e650
mannylopez <manuel.est.lopez@gmail.com>
2020-06-09 00:11:12 -0700

    Test to remove 2 tabs and replace it with 2 spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Load mapbox map
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFubnkiLCJhIjoiMzBCNHFQUSJ9.VQQ9ZSW7viFT1yLhLiWLSA';
-  
+
 var map = new mapboxgl.Map({
   container: 'map', // container id
   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
@@ -10,6 +10,6 @@ var map = new mapboxgl.Map({
 
 // Load stories.geojson file from the data directory
 fetch('../data/stories.geojson')
-	.then(response => response.json())
-	.then(data => console.log(data));
-	// .then(data => console.log(data.features[0]));
+  .then(response => response.json())
+  .then(data => console.log(data));
+  // .then(data => console.log(data.features[0]));
