a5663903bd8194703e63f7ca6e9f32135baf2f2e
mannylopez <manuel.est.lopez@gmail.com>
2020-06-13 16:52:29 -0700

    Create story div with javascript, add in the story title, and append it to the index.html file in the stories pane

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,43 @@ fetch('/news-map/data/stories.geojson')
   // Turn the fetch response into json
   .then(response => response.json())
 
-  // Get the longitude and latitude values from the json
+  // Get the longitude and latitude values from the json, set it to a marker, and add it to the map
   .then(function(data) {
+
     data.features.forEach(function(marker) {
       new mapboxgl.Marker()
         .setLngLat(marker.geometry.coordinates)
         .addTo(map);
     });
+    
+    // Add stories to stories pane
+    // Get value from geojson
+    let text = data.features[0].properties.title
+    
+    // // select the first div with id="#story1"
+    // let title = document.querySelector('#story1');
+    
+    // // Add it to story div
+    // title.innerHTML = text
+  
+
+    // forEach loop
+
+    // 0. Find the 'stories' div
+    let storyPane = document.querySelector('.stories')
+    console.log(storyPane)
+    // 1. Create a new div
+    let div = document.createElement('div')
+    div.textContent = text
+    // 2. Add class="story"
+    // 3. Add to class="stories" div
+    div.setAttribute('class', 'story')
+    // 4. Get title, author, link, source, ...
+    // 5. Add that info to div
+    // 6. Add div to index.html
+    // document.body.container.appendChild(div)
+    console.log(div)
+    storyPane.appendChild(div)
+
     console.log("End of promise block");
-  });
+  })
