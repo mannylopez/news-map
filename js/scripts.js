d29131f9cf8ef8d74110c4754c154be9b1a3ea93
mannylopez <manuel.est.lopez@gmail.com>
2020-06-20 09:21:47 -0700

    Markers and pins are connected. Marker highlighted when the story is selected, and vice versa - Messy code

--- js/scripts.js
+++ js/scripts.js
@@ -22,17 +22,14 @@ function createStory(data) {
   // popup.addClassName('some-class')
 
 // Create a story card
-  let title = data.properties.title;
-  let author = data.properties.author;
-  let publishDate = data.properties.publish_date;
-  let blurb = data.properties.blurb;
-  let source = data.properties.source;
-  let link = data.properties.link;
+
+  // Destructuring
+  const { title, author, publish_date, blurb, source, link, location_name, address } = data.properties;
 
 // Create elements and populate with data from the parameters
-  let div = document.createElement('div');
-  div.setAttribute('class', "story");
-  div.setAttribute('id', title);
+  let storyDiv = document.createElement('div');
+  storyDiv.setAttribute('class', "story");
+  storyDiv.setAttribute('id', title);
 
   let storyLink = document.createElement("a");
   storyLink.setAttribute('href', link);
@@ -42,44 +39,98 @@ function createStory(data) {
   let storyTitle = document.createElement("h3");
 
   let storySourceAndDate = document.createElement("p");
-  storySourceAndDate.textContent = source + " • " + publishDate;
+  storySourceAndDate.textContent = source + " • " + publish_date;
 
   let storyBlurb = document.createElement("p");
   storyBlurb.setAttribute('class', 'blurb');
   storyBlurb.textContent = blurb;
 
+// Add id to marker  
+  // marker.getElement().setAttribute('id', title + " marker")
 
 // Append the elements to the story div
   let storyPane = document.getElementById('stories');
-  storyPane.appendChild(div).appendChild(storyTitle).appendChild(storyLink);
-  storyPane.appendChild(div).appendChild(storyBlurb);
-  storyPane.appendChild(div).appendChild(storySourceAndDate);
+  storyPane.appendChild(storyDiv).appendChild(storyTitle).appendChild(storyLink);
+  storyPane.appendChild(storyDiv).appendChild(storyBlurb);
+  storyPane.appendChild(storyDiv).appendChild(storySourceAndDate);
 
 // When mouse hovers over the map pin, focus the story card
+ 
   marker.getElement().addEventListener('mouseenter', () => {
+
+
     let storyFocus = document.getElementById(title);
     storyFocus.setAttribute('id', 'story-hover');
+
     // marker.togglePopup()
 
-    // new mapboxgl.Marker({color:'#ffd5e5'})
-    // .setLngLat(data.geometry.coordinates)
-    // .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-    //   .setHTML('<h3>' + data.properties.title + '</h3><p>' + data.properties.source + '</p>'))
-    // .addTo(map);
-
+    // marker.remove()
     
+    // new mapboxgl.Marker({color:'#ffd5e5'})
+    //   .setLngLat(data.geometry.coordinates)
+    //   // .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    //   //   .setHTML('<h3>' + data.properties.title + '</h3><p>' + data.properties.source + '</p>'))
+    //   .addTo(map);
+    
+    // marker.togglePopup()
+    
+    // event.target.style.color = 'pink';
   });
+
 
   marker.getElement().addEventListener('mouseleave', () => {
     let storyFocus = document.getElementById('story-hover');
     storyFocus.setAttribute('id', title);
     // marker.togglePopup()
+    // marker.togglePopup()
+
+    // marker.remove()
+
+    // marker = new mapboxgl.Marker({color:'#f5b7b1'})
+    // .setLngLat(data.geometry.coordinates)
+    // .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    //   .setHTML('<h3>' + data.properties.title + '</h3><p>' + data.properties.source + '</p>'))
+    // .addTo(map);
+
   });
 
+  storyDiv.addEventListener('mouseenter', () => {
+    // let abc = document.getElementById(title + ' marker');
+    
+    // console.log(marker.getElement())
+    // marker.getElement().querySelector('g').querySelector('g').setAttribute('class', 'red')
+    // console.log(marker.getElement().querySelector('g').querySelector('g'))
+    // console.log(marker.getElement().querySelector('g'))
+    // console.log(marker.getElement().querySelector('g'[0]))
+    // console.log(marker.getElement().querySelector('g'[1]))
+    // console.log(marker.getElement().querySelector('g'[2]))
+
+    // console.log(marker.getElementByTagName('g'))
+    // console.log(marker.getElement().getElementsByTagName("g")[2])
+    marker.getElement().getElementsByTagName("g")[2].setAttribute('fill', 'black')
+
+  })
+
+  storyDiv.addEventListener('mouseleave', () => {
+    // let abc = document.getElementById(title + ' marker');
+    
+    // console.log(marker.getElement())
+    // marker.getElement().querySelector('g').querySelector('g').setAttribute('class', 'red')
+    // console.log(marker.getElement().querySelector('g').querySelector('g'))
+    // console.log(marker.getElement().querySelector('g'))
+    // console.log(marker.getElement().querySelector('g'[0]))
+    // console.log(marker.getElement().querySelector('g'[1]))
+    // console.log(marker.getElement().querySelector('g'[2]))
+
+    // console.log(marker.getElementByTagName('g'))
+    // console.log(marker.getElement().getElementsByTagName("g")[2])
+    marker.getElement().getElementsByTagName("g")[2].setAttribute('fill', '#3FB1CE')
+
+  })
+
   marker.getElement().addEventListener('click', () => {
-    console.log(marker._color)
-    marker._color = "#ff89b6"
-    console.log(marker._color)
+    
+
   })
 }
 
